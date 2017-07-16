ddaf28aa3d6bd27a7859d16bc1f9c33525b7b69f
Nathan Minchow <nathan.minchow@outlook.com>
2017-07-16 16:29:37 -0500

    Add add todo functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,25 @@
 // To-Do toggle functionality
-$("li").click(function(event) {
+$("ul").on("click", "li", function(event) {
     $(this).toggleClass("completed");
 });
 
 // To-Do delete functionality
-$("span").click(function() {
+$("ul").on("click", "span", function() {
     $(this).parent().fadeOut(200, function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+// Add to-do functionality
+$("input[type='text']").keypress(function(event) {
+    // Check for enter key
+    if(event.which === 13) {
+        // Extract text from field
+        var todoText = $(this).val();
+        // Reset field
+        $(this).val("");
+        // Create a new li
+        $("ul").append("<li><span>X </span>" + todoText + "</li>")
+    }
 });
