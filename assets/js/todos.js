74a1bf26283c751df7857eafaa3d5228683a993c
Nathan Minchow <nathan.minchow@outlook.com>
2017-07-16 16:19:28 -0500

    Add delete functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,12 @@
-// To-Do Toggle Functionality
-$("li").click(function() {
+// To-Do toggle functionality
+$("li").click(function(event) {
     $(this).toggleClass("completed");
+});
+
+// To-Do delete functionality
+$("span").click(function() {
+    $(this).parent().fadeOut(200, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
 });
